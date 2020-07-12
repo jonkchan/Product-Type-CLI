13f500ee958c8e2a853d295ce89f1fe773304cbd
Jonathan Chan <johnkchan252@gmail.com>
2020-07-12 17:56:58 -0400

    Create json_parser.rb

---
+++ lib/json_parser.rb
@@ -0,0 +1,15 @@
+# Class provides methods to parse JSON
+class JSONParser
+  # Method returns parsed JSON array based on file_location argument
+  def self.parse_json(file_location)
+    # Guard Clause: Check if file_location argument exists
+    raise ArgumentError, 'File location does not exist' unless File.exist?(file_location)
+
+    json = File.read(file_location)
+
+    # Guard Clause: Check if json file does not contain data
+    raise ArgumentError, 'No data available in JSON file' unless json.length.positive?
+
+    JSON.parse(json)
+  end
+end
