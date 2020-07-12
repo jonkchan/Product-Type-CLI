77eca7e4da857eb1c5a1b5889b244ff12a2e5794
Jonathan Chan <johnkchan252@gmail.com>
2020-07-12 18:28:23 -0400

    Created Command Line Interface Class & Methods

---
+++ lib/command_line_interface.rb
@@ -0,0 +1,36 @@
+require_relative 'inventory.rb'
+require_relative 'json_parser.rb'
+require 'json'
+require 'set'
+
+# Class provides methods to be used in CLI
+class CommandLineInterface
+  def run(product_type, product_options = [])
+    # Guard Clause: Check if non-empty product_type argument was provided
+    raise ArgumentError, 'Product type argument required' unless product_type
+
+    # Below file_location argument can be updated to process different json dataset
+    json = JSONParser.parse_json('./json/products.json')
+    product_inventory = Inventory.new(json)
+    options_hash = product_inventory.process_available_options(product_type, product_options)
+    display_options(options_hash)
+  end
+
+  private
+
+  # Method displays available product options to end user
+  def display_options(options_hash)
+    # Guard Clause: Check if no product options are available
+    unless options_hash.length.positive?
+      puts 'No options available for specified Product Type / Option(s)'
+      return
+    end
+
+    options_hash.each do |key, value|
+      # Guard Clause: Check if set value is empty
+      next unless value.length.positive?
+
+      puts "#{key.capitalize}: #{value.to_a.join(', ')}"
+    end
+  end
+end
